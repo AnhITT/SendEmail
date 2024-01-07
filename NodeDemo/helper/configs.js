216e4139636348317832fa3341115805bcb5c663
Lam Anh <lamanhkk@gmail.com>
2024-01-07 20:10:09 +0700

    first commit

---
+++ NodeDemo/helper/configs.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SECRET_KEY: "heheheehe",
+    EXP: "20d",
+    cookieExp: 2,
+    MAIL_HOST: "sandbox.smtp.mailtrap.io",
+    MAIL_PORT: 587,
+    MAIL_USER: "api",
+    MAIL_PASS: "2a5569bc80243c6b440fdb097beb584d",
+};
